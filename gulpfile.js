bbca602adc9a056440e9d3d99651a709cc43f8e6
MasatoMakino <makino.masato.g@gmail.com>
2023-02-21 11:30:50 +0900

    update : dependencies

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const { bundleDemo, cleanDemo, watchDemo } = require("gulptask-demo-page").generateTasks({
-    style: `canvas{background-color:#000}`,
-    compileTarget:"es6"
-});
-
-exports.bundleDemo = bundleDemo;
-exports.cleanDemo = cleanDemo;
-exports.watchDemo = watchDemo;
